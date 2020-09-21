1af49141d92c2107c3f46aebbb93ee1c7c6a9c00
EdwardBrodskiy <brodskiedward@gmail.com>
2020-09-21 19:12:34 +0100

    redix rework

--- sorts/redix.py
+++ sorts/redix.py
@@ -1,27 +1,23 @@
-def redix_sort(arr):
+def redix_sort(arr, redix=10):
     digits = len(str(max(arr)))
-
-    arr = list(map(lambda x: str(x).rjust(digits, '0'), arr))
 
     output = [0 for _ in arr]
 
     for digit_pos in reversed(range(digits)):
 
-        counts = [0 for _ in range(10)]
+        counts = [0 for _ in range(redix)]
 
         for num in arr:
-            counts[int(str(num)[digit_pos])] += 1
+            counts[num // redix ** digit_pos % redix] += 1
 
         for i in range(len(counts) - 1):
             counts[i + 1] += counts[i]
 
         for num in reversed(arr):
-            digit = int(num[digit_pos])
+            digit = num // redix ** digit_pos % redix
             counts[digit] -= 1
             output[counts[digit]] = num
 
         arr = output.copy()
-
-    arr = list(map(lambda x: int(x), arr))
 
     return arr
